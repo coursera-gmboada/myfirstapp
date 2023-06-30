79fd79d7aabebf3105dc85db688e821c6ab9539f
Carlos Rodriguez <121172793+crodrigm2@users.noreply.github.com>
2023-06-30 17:23:56 +0000

    Update 2 files

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ function App(props) {
       <h1>Hello, world!</h1>
       <h2>The time now is {currDate.toLocaleTimeString()}.</h2>
       <h2>The Date now is {currDate.toLocaleDateString()}.</h2>
+      <h1>Hola, Mundito!</h1>
+      <h2>La fecha de hoy es: {currDate.toLocaleDateString()}</h2>
+      <h2>La hora actual es:  {currDate.toLocaleTimeString()}</h2>
     </div>
   );
 }
